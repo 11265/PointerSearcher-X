02d613157b6ef67a21d6fca9d720b90e6dadaf3c
11265 <xdllll@163.com>
2024-06-23 21:40:32 +0800

    更新 build.rs

--- command/build.rs
+++ command/build.rs
@@ -22,13 +22,6 @@ fn main() {
 
     const APPLE: [&str; 2] = ["aarch64-apple-darwin", "aarch64-apple-ios"];
     if APPLE.contains(&target.as_str()) {
-        // fuck https://github.com/rust-lang/rust/issues/124462
-        fs::rename(
-            path.join("libptrscan.dylib"),
-            path.join("this.not.static.libptrscan.dylib"),
-        )
-        .unwrap();
-
         println!("cargo:rustc-link-search={}", path.display());
     }
 }
